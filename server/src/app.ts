e664be43821a798b48747040efc5d68fdd06d30e
Dom <dom@overpolish.co>
2024-08-19 13:32:24 +0100

    build: configure app to use CORS and json bodyParser

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,8 +1,32 @@
-import express, { Express, Request, Response } from "express";
+import express, { Express, Request, Response, NextFunction } from "express";
+import cors from "cors";
+import * as bodyParser from "body-parser";
+import routes from "./routes/routes";
 
 const app: Express = express();
+
+/**
+ * App Configuration
+ */
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(routes);
+
 app.get("/", (_req: Request, res: Response) => {
-  res.send("Hello!");
+  res.json({ status: "API is running on /api" });
+});
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  if (err && err.name === "UnauthorizedError") {
+    return res.status(401).json({
+      status: "error",
+      message: "Unauthorized",
+    });
+  } else if (err) {
+    res.status(500).json(err.message);
+  }
 });
 
 export default app;
--- server/src/index.ts
+++ server/src/index.ts
@@ -3,5 +3,5 @@ import app from "./app";
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-  return console.log(`Express is listening at http://localhost:${port}`);
+  return console.log(`server running on port: ${port}`);
 });
