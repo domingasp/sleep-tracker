f1b959ce0427f77572901b83f04c2b6cb66e349b
domingasp <d.pleck@outlook.com>
2024-08-19 13:32:24 +0100

    build: configure app to use CORS and json bodyParser

--- server/src/__tests__/index.test.ts
+++ server/src/__tests__/index.test.ts
@@ -1,9 +1,9 @@
 import request from "supertest";
 import app from "../app";
 
-test('root responds with "Hello!"', async () => {
+test('root path responds with "API is running on /api"', async () => {
   const res = await request(app).get("/");
 
   expect(res.statusCode).toEqual(200);
-  expect(res.text).toBe("Hello!");
+  expect(res.body).toEqual({ status: "API is running on /api" });
 });
