1545aab91ea2eb0a397bacec8035a4b18ee7454e
Dom <dom@overpolish.co>
2024-08-20 11:20:21 +0100

    feat: add user list display in table

---
+++ client/src/app/routes/landing.test.tsx
@@ -0,0 +1,28 @@
+import "@testing-library/jest-dom";
+import { render, screen } from "../../testing-utils";
+import { LandingRoute } from "./landing";
+import * as users from "../../features/users/api/get-users";
+
+test("landing route must display the app name, a button for adding sleep, and user table", () => {
+  jest.spyOn(users, "useUsers").mockReturnValue({
+    data: [{ id: 1, name: "John", entryCount: 10 }],
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } as any); // TODO proper mocking of UseQueryResult
+
+  render(<LandingRoute />);
+
+  const titleElement = screen.getByTestId("content-layout-title");
+  expect(titleElement).toHaveTextContent("Sleep Tracker");
+
+  const trackSleepButtonElement = screen.getByTestId(
+    "landing-route-track-sleep-button"
+  );
+  expect(trackSleepButtonElement).toHaveTextContent("Track Sleep");
+
+  const tableHintElement = screen.getByTestId("users-list-table-hint");
+  expect(tableHintElement).toHaveTextContent(
+    "Click a row to see sleep history for the last 7 days"
+  );
+
+  // TODO check mantine react table rendered
+});
--- client/src/app/routes/landing.tsx
+++ client/src/app/routes/landing.tsx
@@ -1,7 +1,9 @@
-import { Stack, Title } from "@mantine/core";
 import { getUsersQueryOptions } from "../../features/users/api/get-users";
 import { QueryClient } from "@tanstack/react-query";
 import { UsersList } from "../../features/users/components/users-list";
+import { ContentLayout } from "../../components/layouts/content-layout";
+import { Button } from "@mantine/core";
+import { IconBedFilled } from "@tabler/icons-react";
 
 // eslint-disable-next-line react-refresh/only-export-components
 export const usersLoader = (queryClient: QueryClient) => async () => {
@@ -15,9 +17,19 @@ export const usersLoader = (queryClient: QueryClient) => async () => {
 
 export function LandingRoute() {
   return (
-    <Stack>
-      <Title>Hello world!</Title>
+    <ContentLayout
+      topRightActions={
+        <Button
+          leftSection={<IconBedFilled />}
+          variant="outline"
+          color="orange"
+          data-testid="landing-route-track-sleep-button"
+        >
+          Track Sleep
+        </Button>
+      }
+    >
       <UsersList />
-    </Stack>
+    </ContentLayout>
   );
 }
